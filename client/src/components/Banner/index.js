45e9b5b20f70698ab330fc0a9fb090ecdc1282bf
Aaron Diggdon <aarondiggdon@gmail.com>
2020-11-19 20:51:47 -0800

    Added components and styles

---
+++ client/src/components/Banner/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+
+function Banner() {
+  return (
+    <nav className="navbar navbar-expand-lg d-flex justify-content-center">
+      <h1 className="header d-flex justify-content-center">
+        React Google Books
+      </h1>
+      
+    </nav>
+  );
+}
+
+export default Banner;
