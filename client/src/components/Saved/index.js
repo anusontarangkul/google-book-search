012d8888fac67b635335ea2d9d139bd52b7771a0
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-11-21 13:42:28 -0800

    fix css bug

--- client/src/components/Saved/index.js
+++ client/src/components/Saved/index.js
@@ -1,36 +1,36 @@
 import React from "react";
 import "./style.css";
 
-function Saved({saved, handleDelete}) {
+function Saved({ saved, handleDelete }) {
 
-  return <article className="savedContainer container-fluid d-flex justify-content-center">
-  
-  {saved.map((cards, index)  => (
-    <div key={index} class="card">
-      <div class="body">
-        <div class="row">
-          <div class="col-8 header">
-            <h3 class="title">{cards.title}</h3>
-            <h5 class="author">By {cards.authors}</h5>
-            
+  return <article className="savedContainer container-fluid justify-content-center">
+
+    {saved.map((cards, index) => (
+      <div key={index} class="card">
+        <div class="body">
+          <div class="row">
+            <div class="col-8 header">
+              <h3 class="title">{cards.title}</h3>
+              <h5 class="author">By {cards.authors}</h5>
+
+            </div>
+            <div class="col-4">
+              <button class="delete btn btn-outline-danger" onClick={event => handleDelete(event, cards._id)}>Delete</button>
+              <a href={cards.link} target="_blank"><button target="_blank" class="view btn btn-outline-primary">View</button></a>
+            </div>
+            <hr />
           </div>
-          <div class="col-4">
-            <button class="delete btn btn-outline-danger" onClick = {event => handleDelete(event, cards._id)}>Delete</button>
-            <a href = {cards.link} target= "_blank"><button target= "_blank" class="view btn btn-outline-primary">View</button></a>
-          </div>
-          <hr/>
+
+          <a href={cards.link} target="_blank">
+            <img class="image" src={cards.image} alt={cards.title} />
+          </a>
+          <p class="text">{cards.description}</p>
+
         </div>
-
-        <a href = {cards.link} target= "_blank">
-        <img class="image" src={cards.image} alt = {cards.title}/>
-        </a>
-        <p class="text">{cards.description}</p>
-
       </div>
-    </div>
-   ))}
-  
-</article>;
+    ))}
+
+  </article>;
 }
 
 export default Saved;
