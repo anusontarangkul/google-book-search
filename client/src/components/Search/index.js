5c09c3eb6fee08efb90f8e002766b57db1b59564
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-11-21 14:15:13 -0800

    clear after search

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,23 +1,26 @@
-import React, {useRef} from "react";
+import React, { useRef } from "react";
 import "./style.css";
 
 function Search(props) {
   const inputRef = useRef();
   //console.log(inputRef.current.value)
   return (
-  <div>
-    <div class="container d-flex justify-content-center">
-      <form onSubmit={(event) => props.handleSearch(event, inputRef.current.value) }>
-        <input 
-        type="text" 
-        ref ={inputRef}  
-        className="input" 
-        placeholder="Search Books" />
-      </form>
+    <div>
+      <div class="container d-flex justify-content-center">
+        <form onSubmit={(event) => {
+          props.handleSearch(event, inputRef.current.value);
+          inputRef.current.value = "";
+        }}>
+          <input
+            type="text"
+            ref={inputRef}
+            className="input"
+            placeholder="Search Books" />
+        </form>
+      </div>
     </div>
-  </div>
   )
-  
+
 }
 
 export default Search;
