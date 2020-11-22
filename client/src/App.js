1d84face9da66331bb7c4eefaf708d8a0c740e39
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-11-21 16:29:32 -0800

    update readme

--- client/src/App.js
+++ client/src/App.js
@@ -11,26 +11,26 @@ import GoogleBooks from "./utils/GetBooks";
 import API from "./utils/api";
 
 function App() {
-  
-  const [resultState, setResultState] = useState([]);
-  const [savedState , setSavedState] = useState([]);
 
-  useEffect(()=>{
+  const [resultState, setResultState] = useState([]);
+  const [savedState, setSavedState] = useState([]);
+
+  useEffect(() => {
     getBooksDb();
-  },[]);
-  
-  const getBooksDb = () =>{
+  }, []);
+
+  const getBooksDb = () => {
     API.getBooks()
-    .then(res => {
-    setSavedState(res.data);
-    }).catch(err => { console.log(err) });
+      .then(res => {
+        setSavedState(res.data);
+      }).catch(err => { console.log(err) });
   }
   const handleSearch = (event, searchItem) => {
     event.preventDefault();
-    //console.log(searchItem);
+
     GoogleBooks.searchTerms(searchItem)
       .then(res => {
-        //console.log(res);
+
         setResultState([...resultState, {
           title: res.data.items[0].volumeInfo.title,
           authors: res.data.items[0].volumeInfo.authors,
@@ -38,44 +38,44 @@ function App() {
           image: res.data.items[0].volumeInfo.imageLinks.thumbnail,
           link: res.data.items[0].volumeInfo.infoLink
         }])
-        
+
       }).catch(err => { console.log(err) });
   }
-  //by id of result state push books to db
-  const handleSave = (event, index) =>{
+
+  const handleSave = (event, index) => {
     event.preventDefault();
     console.log("hit saved");
     console.log(resultState[index]);
     API.saveBook(resultState[index])
-    .then(res =>{
-      console.log("saved");
-    }).catch(err => { console.log(err) });
+      .then(res => {
+        console.log("saved");
+      }).catch(err => { console.log(err) });
   }
-  const handleDelete = (event, id) =>{
+  const handleDelete = (event, id) => {
     event.preventDefault();
     console.log("hit delete");
     console.log(id);
     API.deleteBook(id)
-    .then(res =>{
-      console.log("delete");
-      getBooksDb();
-    }).catch(err => { console.log(err) });
+      .then(res => {
+        console.log("delete");
+        getBooksDb();
+      }).catch(err => { console.log(err) });
   }
 
   return (
     <Router>
       <Wrapper>
-          <Banner />
-          <Switch>
-          <Route exact path= "/">
+        <Banner />
+        <Switch>
+          <Route exact path="/">
             <Search handleSearch={handleSearch} />
-            <Results results={resultState} handleSave = {handleSave}/>
+            <Results results={resultState} handleSave={handleSave} />
           </Route>
-          <Route exact path= "/saved">
-            <SavedBanner/>
-            <Saved saved = {savedState} handleDelete = {handleDelete}/>
+          <Route exact path="/saved">
+            <SavedBanner />
+            <Saved saved={savedState} handleDelete={handleDelete} />
           </Route>
-          </Switch>
+        </Switch>
       </Wrapper>
     </Router>
   );
